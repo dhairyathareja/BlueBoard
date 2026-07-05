7c45c047a724221a30cd25ddd8b71c6063704cb3
Dhairya Thareja <dhairyathareja@gmail.com>
2026-07-06 01:18:03 +0530

    Initializing Project: Setting Up neccesaary packages

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+// Import Packages
+import mongoose from "mongoose";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+
+
+// Import Routes
+import authRouter from "./src/routes/auth.route.js";
+
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(bodyParser.json({ limit: "4kb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "4kb" }));
+app.use(cookieParser());
+
+
+// Routing APIs
+app.use('/auth',authRouter);
+
+
+
+const PORT = process.env.PORT || 4444;
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        app.listen(PORT,()=>{
+            console.log(`Server Started at ${PORT}`);
+        });        
+    })
+    .catch(err => console.log("❌ DB Error:", err));    
+
