43f5056e047f5209cd24184fc0129854d609326d
Dhairya Thareja <dhairyathareja@gmail.com>
2026-07-29 03:10:21 +0530

    Frontend Production deployment changes

--- backend/src/routes/employee.route.js
+++ backend/src/routes/employee.route.js
@@ -1,14 +1,20 @@
 import express from "express";
 
-import { getDashboard, getEmployeeDetails, getEmployeeList, postUpdateEmployee, postUpdateStatus } from "../controller/employee.controller.js";
+import {
+  getDashboard,
+  getEmployeeDetails,
+  getEmployeeList,
+  postUpdateEmployee,
+  postUpdateStatus,
+} from "../controller/employee.controller.js";
 
 const router = express.Router();
 
-router.get("/list",getEmployeeList);
-router.get("/details/:id",getEmployeeDetails);
-router.get("/dashboardStats",getDashboard);
+router.get("/list", getEmployeeList);
+router.get("/details/:id", getEmployeeDetails);
+router.get("/dashboardStats", getDashboard);
 
-router.post("/update",postUpdateEmployee);
-router.post("/update/status",postUpdateStatus);
+router.post("/update", postUpdateEmployee);
+router.post("/update/status", postUpdateStatus);
 
 export default router;
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -4,14 +4,24 @@ output "ec2_public_ip" {
 
 }
 
-/*
+
 output "cloudfront_url" {
   value = aws_cloudfront_distribution.frontend.domain_name
 }
-*/
+
 
 output "frontend_bucket" {
 
   value = aws_s3_bucket.frontend.bucket
 
+}
+
+output "cloudfront_domain" {
+  description = "CloudFront Distribution Domain Name"
+  value       = aws_cloudfront_distribution.frontend.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "CloudFront Distribution ID"
+  value       = aws_cloudfront_distribution.frontend.id
 }
