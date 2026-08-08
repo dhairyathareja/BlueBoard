e88bc404776a439c295af9c4b28612a92c590a62
Dhairya Thareja <dhairyathareja@gmail.com>
2026-08-09 03:47:51 +0530

    Frontend Trigger Test

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -30,3 +30,33 @@ resource "aws_s3_bucket_public_access_block" "frontend_block" {
   restrict_public_buckets = true
 
 }
+
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontRead"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action = "s3:GetObject"
+
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+          }
+        }
+      }
+    ]
+  })
+}
