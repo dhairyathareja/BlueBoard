badf2fe0112f6c034e0e9844c8660a4edbf30f8b
Dhairya Thareja <dhairyathareja@gmail.com>
2026-08-09 10:57:40 +0530

    S3 Client Fix

--- backend/src/config/s3.config.js
+++ backend/src/config/s3.config.js
@@ -4,13 +4,13 @@ const s3Client = new S3Client({
 
     region: process.env.AWS_REGION,
 
-    credentials: {
+    // credentials: {
 
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 
-    }
+    // }
 
 });
 
